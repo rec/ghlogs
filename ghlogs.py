c468074084ba6bf1aa52d01948602d0f9437a026
Tom Ritchford <tom@swirly.com>
2024-05-19 15:22:33 +0200

    Create summary file

--- ghlogs.py
+++ ghlogs.py
@@ -13,11 +13,12 @@ FAILURE = 'failure'
 CONCLUSION = 'conclusion'
 
 START = 20 * '!'
-STOP = 'The above exception was the direct cause of the following exception'
+SPLIT = 80 * '-'
+ABOVE_EXCEPTION = 'The above exception was the direct cause of the following exception'
 COMMAND = 'To execute this test, run the following from the base repo dir'
 
 PATTERNS = [
-    ['basic', START, STOP],
+    ['basic', START, ABOVE_EXCEPTION],
 ]
 
 MIN_LINES = 250
@@ -63,7 +64,9 @@ def filter_job_log(lines):
     for name, start, stop in PATTERNS:
         between = list(lines_between(lines, start, stop, MIN_LINES))
         if between:
-            result = (f'Pattern: {name}\n\n', *between[:MAX_LINES])
+            result = between[:MAX_LINES]
+            if len(PATTERNS) > 1:
+                result = f'Pattern: {name}\n\n', *result
             break
 
     return ''.join(result)
@@ -82,24 +85,33 @@ def write_log(job, log_dir):
     lines = log.splitlines(keepends=True)
 
     write('full', log)
-    write('summary', filter_job_log(lines))
+    summary = filter_job_log(lines)
+    write('summary', summary)
 
     cmd_index = next(i for i, li in enumerate(lines) if COMMAND in li)
     _, cmd = lines[cmd_index + 1].split(maxsplit=1)
-    return f'{cmd.strip()}  # {job_id}\n'
+    return summary, f'{cmd.strip()}  # {job_id}\n'
 
 
 def write_logs(run_id, log_dir='log'):
     log_dir = pathlib.Path(log_dir) / run_id
     log_dir.mkdir(parents=True, exist_ok=True)
     failures = get_failures(run_id)
-    commands = ''.join(write_log(job, log_dir) for job in failures)
+    summaries_cmds = [write_log(job, log_dir) for job in failures]
+
+    with (log_dir / f'{run_id}-summary.txt').open('w') as fp:
+        for i, (summary, cmd) in enumerate(summaries_cmds):
+            if i:
+                fp.write('\n')
+
+            fp.write(f'BEGIN {cmd}\n\n')
+            fp.writelines(summary)
+            fp.write(f'\nEND {cmd}\n{SPLIT}\n')
 
     cmd_file = log_dir / 'commands.sh'
     with cmd_file.open('w') as fp:
         fp.write('#!/bin/bash\n\nset -e\n\n')
-        for c in commands:
-            fp.write(c)
+        fp.writelines(cmd for _, cmd in summaries_cmds)
 
     cmd_file.chmod(cmd_file.stat().st_mode | 0o111)
 
